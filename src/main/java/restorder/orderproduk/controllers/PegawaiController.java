cbc732d8e50e6f18709e539868198c38fea058f7
mufidbrown <ilhammufid67@gmail.com>
2023-10-18 16:56:57 +0700

    update controller & service class

---
+++ src/main/java/restorder/orderproduk/controllers/PegawaiController.java
@@ -0,0 +1,60 @@
+package restorder.orderproduk.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import restorder.orderproduk.entity.Pegawai;
+import restorder.orderproduk.service.PegawaiService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pegawai")
+public class PegawaiController {
+
+
+    @Autowired
+    private PegawaiService pegawaiService;
+
+    @GetMapping
+    public ResponseEntity<List<Pegawai>> getAllPegawais() {
+        List<Pegawai> pegawais = pegawaiService.getAllPegawais();
+        return new ResponseEntity<>(pegawais, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pegawai> getProductById(@PathVariable Long id) {
+        Pegawai pegawai = pegawaiService.getPegawaiById(id);
+        if (pegawai != null) {
+            return ResponseEntity.ok(pegawai);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Pegawai> createProduct(@RequestBody Pegawai pegawai) {
+        Pegawai createdPegawai = pegawaiService.createPegawai(pegawai);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPegawai);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pegawai> updatePegawai(@PathVariable Long id, @RequestBody Pegawai pegawai) {
+        Pegawai updatedPegawai = pegawaiService.updatePegawai(id, pegawai);
+        if (updatedPegawai != null) {
+            return ResponseEntity.ok(updatedPegawai);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deletePegawai(@PathVariable Long id) {
+        pegawaiService.deletePegawai(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
--- src/main/java/restorder/orderproduk/service/PegawaiService.java
+++ src/main/java/restorder/orderproduk/service/PegawaiService.java
@@ -1,12 +1,9 @@
 package restorder.orderproduk.service;
 
-import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import restorder.orderproduk.entity.Pegawai;
-import restorder.orderproduk.entity.Product;
 import restorder.orderproduk.repositories.PegawaiRepository;
-import restorder.orderproduk.repositories.ProductRepository;
 
 import java.util.List;
 
@@ -15,7 +12,7 @@ public class PegawaiService {
 
     @Autowired
     private PegawaiRepository pegawaiRepository;
-    public List<Pegawai> getAllPegawai() {
+    public List<Pegawai> getAllPegawais() {
         return pegawaiRepository.findAll();
     }
 
@@ -30,6 +27,11 @@ public class PegawaiService {
     }
 
 
+//    public boolean deletePegawai(Long id) {
+//        pegawaiRepository.deleteById(id);
+//        return false;
+//    }
+
     public void deletePegawai(Long id) {
         pegawaiRepository.deleteById(id);
     }
@@ -37,8 +39,7 @@ public class PegawaiService {
     public Pegawai createPegawai(Pegawai pegawai) {
         pegawaiRepository.save(pegawai);
         return pegawai;
-    }
-
+   }
 
 
 }
