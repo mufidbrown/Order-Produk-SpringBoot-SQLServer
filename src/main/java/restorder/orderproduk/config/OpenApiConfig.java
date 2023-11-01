fde9fafd36428b0ede68d96187ff2144d080b3a3
mufidbrown <ilhammufid67@gmail.com>
2023-11-01 09:47:13 +0700

    create OpenAPI 3 swagger UI

---
+++ src/main/java/restorder/orderproduk/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package restorder.orderproduk.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Ilham",
+                        email = "contact@ilhammufid67@gmail.com",
+                        url = "http://localhost:8080"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi spesification - IlhamMufid",
+                version = "2.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "http://springdoc.org"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+public class OpenApiConfig {
+
+
+
+}
