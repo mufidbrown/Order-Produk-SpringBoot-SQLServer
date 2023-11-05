f3726d7cf40f209d04a0c0d399903268ee202064
mufidbrown <ilhammufid67@gmail.com>
2023-11-05 20:27:17 +0700

    update fake login at JWT

--- src/main/java/restorder/orderproduk/security/JwtIssuer.java
+++ src/main/java/restorder/orderproduk/security/JwtIssuer.java
@@ -1,15 +1,23 @@
 package restorder.orderproduk.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
-
-import static org.springframework.security.config.Elements.JWT;
 
 @Component
 public class JwtIssuer {
 
     public String issue(long userId, String email, List<String> roles){
         return JWT.create()
+                .withSubject(String.valueOf(userId))
+                .withExpiresAt(Instant.now().plus(Duration.of(1, ChronoUnit.DAYS)))
+                .withClaim("e", email)
+                .withClaim("a", roles)
+                .sign(Algorithm.HMAC256("secret"));
     }
 }
