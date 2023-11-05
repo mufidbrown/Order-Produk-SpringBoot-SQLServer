1aa46da9a25c9ba23b6d0759b1cc457be3a3735d
mufidbrown <ilhammufid67@gmail.com>
2023-11-05 19:48:09 +0700

    update security

--- src/main/java/restorder/orderproduk/controllers/HelloController.java
+++ src/main/java/restorder/orderproduk/controllers/HelloController.java
@@ -1,4 +1,15 @@
 package restorder.orderproduk.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello, World";
+    }
 }
