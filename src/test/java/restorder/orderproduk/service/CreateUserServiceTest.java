daa418fc4d228c42fbac449318e0b226fbc5fd85
mufidbrown <ilhammufid67@gmail.com>
2023-11-14 13:49:30 +0700

    success create UserServiceTest

--- src/test/java/restorder/orderproduk/service/CreateUserServiceTest.java
+++ src/test/java/restorder/orderproduk/service/CreateUserServiceTest.java
@@ -1,4 +1,37 @@
 package restorder.orderproduk.service;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import restorder.orderproduk.entity.User;
+import restorder.orderproduk.repositories.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CreateUserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void whenSaveUser_shouldReturnUser() {
+        User user = new User();
+        user.setUsername("Test Name");
+
+        when(userRepository.save(ArgumentMatchers.any(User.class))).thenReturn(user);
+
+        User created = userService.createUser(user);
+
+        assertThat(created.getUsername()).isSameAs(user.getUsername());
+        verify(userRepository).save(user);
+    }
 }
