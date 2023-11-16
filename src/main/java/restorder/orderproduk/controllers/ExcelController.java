8ab417443d627b3c955437a219c41a8773d3fd03
mufidbrown <ilhammufid67@gmail.com>
2023-11-16 09:41:47 +0700

    Configure Multipart File for Servlet

--- src/main/java/restorder/orderproduk/controllers/ExcelController.java
+++ src/main/java/restorder/orderproduk/controllers/ExcelController.java
@@ -1,4 +1,42 @@
 package restorder.orderproduk.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import restorder.orderproduk.exception.ExcelHelper;
+import restorder.orderproduk.message.ResponseMessage;
+import restorder.orderproduk.service.ExcelService;
+
+@Controller
+@RequestMapping("/api/excel")
 public class ExcelController {
+
+    @Autowired
+    ExcelService fileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+
+        if (ExcelHelper.hasExcelFormat(file)) {
+            try {
+                fileService.save(file);
+
+                message = "Uploaded the file successfully: " + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+            } catch (Exception e) {
+                message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
+        }
+
+        message = "Please upload an excel file!";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+    }
 }
