f1a06cfe15dde370774c1226652d07e13afd785d
mufidbrown <ilhammufid67@gmail.com>
2023-11-22 14:20:05 +0700

    final & success create Import Excel in Spring Boot

--- src/main/java/restorder/orderproduk/entity/Customer.java
+++ src/main/java/restorder/orderproduk/entity/Customer.java
@@ -3,6 +3,7 @@ package restorder.orderproduk.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "customers")
 public class Customer {
 
     @Id
