ade00aaf1d95ab669102af18c2a388130a03e9f1
mufidbrown <ilhammufid67@gmail.com>
2023-11-23 12:43:15 +0700

    update UserServiceTest

--- src/test/java/restorder/orderproduk/service/UserServiceTest.java
+++ src/test/java/restorder/orderproduk/service/UserServiceTest.java
@@ -1,7 +1,22 @@
 package restorder.orderproduk.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import restorder.orderproduk.entity.User;
+import restorder.orderproduk.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+//import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -21,8 +36,8 @@ public class UserServiceTest {
     public void getAllUsers() {
         //given
         List<User> users = new ArrayList<>();
-        users.add(new User(1L, "user1", "user1@example.com"));
-        users.add(new User(2L, "user2", "user2@example.com"));
+        users.add(new User(1L, "John", "Jakarta", "john.doe@example.com", "081312344556", "secret12345678"));
+        users.add(new User(2L, "John", "Jakarta", "john.doe@example.com", "081312344556", "secret12345678"));
 
         when(userRepository.findAll()).thenReturn(users);
 
@@ -33,14 +48,15 @@ public class UserServiceTest {
         assertThat(found).isNotNull();
         assertThat(found).hasSize(2);
         assertThat(found.get(0).getId()).isEqualTo(1L);
-        assertThat(found.get(0).getName()).isEqualTo("user1");
-        assertThat(found.get(0).getEmail()).isEqualTo("user1@example.com");
+        assertThat(found.get(0).getUsername()).isEqualTo("John");
+        assertThat(found.get(0).getAlamat()).isEqualTo("Jakarta");
+        assertThat(found.get(0).getEmail()).isEqualTo("john.doe@example.com");
     }
 
     @Test
     public void getUserById() {
         //given
-        User user = new User(1L, "user1", "user1@example.com");
+        User user = new User(1L,"John", "Jakarta", "john.doe@example.com", "081312344556", "secret12345678");
 
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
@@ -50,14 +66,15 @@ public class UserServiceTest {
         //then
         assertThat(found).isNotNull();
         assertThat(found.getId()).isEqualTo(1L);
-        assertThat(found.getName()).isEqualTo("user1");
+        assertThat(found.getUsername()).isEqualTo("user1");
+        assertThat(found.getAlamat()).isEqualTo("Jakarta");
         assertThat(found.getEmail()).isEqualTo("user1@example.com");
     }
 
     @Test
     public void createUser() {
         //given
-        User user = new User(null, "user1", "user1@example.com");
+        User user = new User(null, "John", "Jakarta", "john.doe@example.com", "081312344556", "secret12345678");
 
         when(userRepository.save(user)).thenReturn(user);
 
@@ -67,14 +84,15 @@ public class UserServiceTest {
         //then
         assertThat(created).isNotNull();
         assertThat(created.getId()).isNull();
-        assertThat(created.getName()).isEqualTo("user1");
+        assertThat(created.getUsername()).isEqualTo("user1");
+        assertThat(created.getUsername()).isEqualTo("jakarta");
         assertThat(created.getEmail()).isEqualTo("user1@example.com");
     }
 
     @Test
     public void updateUser() {
         //given
-        User user = new User(1L, "user1", "user1@example.com");
+        User user = new User(1L, "John", "Jakarta", "john.doe@example.com", "081312344556", "secret12345678");
 
         when(userRepository.save(user)).thenReturn(user);
 
@@ -84,14 +102,15 @@ public class UserServiceTest {
         //then
         assertThat(updated).isNotNull();
         assertThat(updated.getId()).isEqualTo(1L);
-        assertThat(updated.getName()).isEqualTo("user1");
+        assertThat(updated.getUsername()).isEqualTo("user1");
+        assertThat(updated.getAlamat()).isEqualTo("jakarta");
         assertThat(updated.getEmail()).isEqualTo("user1@example.com");
     }
 
     @Test
     public void deleteUser() {
         //given
-        User user = new User(1L, "user1", "user1@example.com");
+        User user = new User(1L, "John", "Jakarta", "john.doe@example.com", "081312344556", "secret12345678");
 
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
