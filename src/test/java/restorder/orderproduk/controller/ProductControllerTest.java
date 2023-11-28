958814eb13f8ac68f0dd1fc273bca092d6a7953b
mufidbrown <ilhammufid67@gmail.com>
2023-11-28 09:23:51 +0700

    update ProductControllerTest

--- src/test/java/restorder/orderproduk/controller/ProductControllerTest.java
+++ src/test/java/restorder/orderproduk/controller/ProductControllerTest.java
@@ -1,78 +1,60 @@
-//package restorder.orderproduk.controller;
-//
-//
-//import org.hamcrest.Matchers;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//import restorder.orderproduk.api.BaseResponse;
-//import restorder.orderproduk.controllers.ProductController;
-//import restorder.orderproduk.controllers.UserController;
-//import restorder.orderproduk.entity.Product;
-//import restorder.orderproduk.service.ProductService;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(controllers = ProductController.class)
-////@WebMvcTest(ProductController.class)
-//public class ProductControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private ProductService productService;
-//
-//    // Contoh method untuk menguji pengambilan semua produk
-//    @Test
-//    public void getAllProducts_ReturnsListOfProducts() throws Exception {
-//        // Buat objek tampilan dari produk
-//        Product product1 = new Product(5L, "Product 1", 1, 10000,"Description 1");
-//        Product product2 = new Product(6L, "Product 2", 2,  20000, "Description 2");
-//
-//        // Buat objek tampilan dari semua produk
-//        List<Product> products = Arrays.asList(product1, product2);
-//
-//        // Buat objek tampilan dari respons
-//        BaseResponse<List<Product>> response = BaseResponse.ok("Daftar Semua Produk", products);
-//
-//        // Mock objek tampilan dari ProductService
-//        when(productService.getAllProducts()).thenReturn(products);
-//
-//        // Jalankan permintaan HTTP GET
-//        mockMvc.perform(MockMvcRequestBuilders.get("/product/v1"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.status", Matchers.is("OK")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Daftar Semua Produk")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data", Matchers.hasSize(2)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].id", Matchers.is(1)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].name", Matchers.is("Product 1")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].description", Matchers.is("Description 1")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].price", Matchers.is(10000)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].id", Matchers.is(2)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].name", Matchers.is("Product 2")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].description", Matchers.is("Description 2")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.data[1].price", Matchers.is(20000)));
-//    }
-//}
-//
-//
-//
-//
-//
-//
-//
+package restorder.orderproduk.controller;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import restorder.orderproduk.controllers.ProductController;
+import restorder.orderproduk.entity.Product;
+import restorder.orderproduk.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class ProductControllerTest {
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductService productService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetAllProducts() {
+        List<Product> productList = new ArrayList<>();
+        // Add some dummy products to the list or use Mockito to mock product data
+
+        when(productService.getAllProducts()).thenReturn(productList);
+
+        ResponseEntity<List<Product>> responseEntity = productController.getAllProducts();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(productList, responseEntity.getBody());
+    }
+
+
+
+
+
+
+
+
+    
+    // untuk inject dependencies Unit Test nya pakai JunitTest / JupiterTest? soalnya di pom.xml
+    //terdapat dependencies Junit & Jupiter
+
+
+
+}
--- src/test/java/restorder/orderproduk/controller/UserControllerTest.java
+++ src/test/java/restorder/orderproduk/controller/UserControllerTest.java
@@ -1,19 +1,30 @@
 package restorder.orderproduk.controller;
 
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import restorder.orderproduk.controllers.UserController;
 import restorder.orderproduk.entity.User;
 import restorder.orderproduk.service.UserService;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 public class UserControllerTest {
+
 
 
         @InjectMocks
@@ -21,6 +32,10 @@ public class UserControllerTest {
 
         @Mock
         private UserService userService;
+
+
+
+
 
 
 //        @Test
@@ -37,12 +52,28 @@ public class UserControllerTest {
 //        }
 
 
+
+//    @Test
+//    public void testGetUserById() throws Exception {
+//        Long userId = 1L;
+//        User user = new User(1L,"anjali", "tebet", "anjali123@gmail.com", "081312345678", "secret1234");
+//
+//        when(userService.getUserById(userId)).thenReturn(user);
+//
+//        mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", userId))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("Anjali")));
+//    }
+
+
+
         @Test
         public void  testCreateUser() throws Exception{
             User input = new User(1L,"anjali", "tebet", "anjali123@gmail.com", "081312345678", "secret1234");
             User returned = new User(1L,"anjali", "tebet", "anjali123@gmail.com", "081312345678", "secret1234");
             //stub the data
-            Mockito.when(userService.createUser(input)).thenReturn(returned);
+            when(userService.createUser(input)).thenReturn(returned);
 
             //original method call
             User result = userController.createUser(input).getBody();
@@ -56,7 +87,7 @@ public class UserControllerTest {
             User input = new User(1L, "rahul", "mampang", "rahul123@gmail.com", "081387654321", "secret4321");
             User returned = new User(1L,"rahul_updated",  "mampang", "rahul123@gmail.com", "081387654321", "secret4321");
             //stub the data
-            Mockito.when(userService.updateUser(1L,input)).thenReturn(returned);
+            when(userService.updateUser(1L,input)).thenReturn(returned);
 
             //original method call
             User result = userController.updateUser(1L,input).getBody();
