6f65f9c97ea261ff7624b579c05db464e29e8d56
mufidbrown <ilhammufid67@gmail.com>
2023-12-04 08:21:15 +0700

    Add UserRepositoryTest

--- src/main/java/restorder/orderproduk/repositories/UserRepository.java
+++ src/main/java/restorder/orderproduk/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package restorder.orderproduk.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import restorder.orderproduk.entity.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByEmail(String email);
 //    String getId();
 //    String getUsername();
 //    String getPassword();
-
 }
---
+++ src/test/java/restorder/orderproduk/repository/UserRepositoryTest.java
@@ -0,0 +1,7 @@
+package restorder.orderproduk.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserRepositoryTest {
+}
