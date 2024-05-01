4a026c8cd1bdda79ff2e55ef748ead2235772d0c
mufidbrown <ilhammufid67@gmail.com>
2024-05-01 19:24:40 +0700

    update all

--- src/main/java/restorder/orderproduk/entity/User.java
+++ src/main/java/restorder/orderproduk/entity/User.java
@@ -26,54 +26,54 @@ public class User {
     private String password;
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getTelepon() {
-        return telepon;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setTelepon(String telepon) {
-        this.telepon = telepon;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public String getAlamat() {
+//        return alamat;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public String getTelepon() {
+//        return telepon;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public void setAlamat(String alamat) {
+//        this.alamat = alamat;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public void setTelepon(String telepon) {
+//        this.telepon = telepon;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 
 
 }
